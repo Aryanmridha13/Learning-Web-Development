913d0b4fb912de9629b282d199a673490bdd0dca
Aryanmridha13 <aryanmridha8@gmail.com>
2024-12-05 19:58:13 +0530

    js

---
+++ cond.js
@@ -0,0 +1,20 @@
+let num = parseInt(prompt("Enter 1 number :"))
+
+
+let num2= num
+
+
+if(num%2==0){
+    console.log("even:",num2);
+    
+}else{
+    console.log("odd :",num2);
+    
+}
+let areaCiecle = 3.14*num2*num2
+console.log(areaCiecle);
+
+let c = num2*num2*num2
+console.log(c);
+
+
---
+++ cond1.js
@@ -0,0 +1,11 @@
+let num =parseInt(prompt("Enter num"))
+if(num>0){
+    console.log("+ve num :",num);
+    
+}else if(num<0){
+    console.log("-ve num :",num);
+    
+}else{
+    console.log("nutral num :",num);
+    
+}
