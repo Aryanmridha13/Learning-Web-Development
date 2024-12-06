24d45b65c788c76ba2c31dc8ee0b56a5a49349b2
Aryanmridha13 <aryanmridha8@gmail.com>
2024-12-06 19:57:42 +0530

    js

---
+++ nastedif.js
@@ -0,0 +1,37 @@
+// let num=parseInt(prompt("Enter a number :"))
+
+// if(num>=18){
+    
+//     let n = parseInt(prompt("Enter number 1 to indian"))
+//     if(n==1){
+//         alert("You are elegible")
+//     }
+//     else{
+//         alert("You are not ihdian")
+//     }
+    
+// }
+// else{
+//     alert("You are a child")
+// }
+
+
+// let n = parseInt(prompt("Enter a number :"))
+
+// switch(n){
+//     case 1:
+//         alert("C++")
+//         break;
+//     case 2 :
+//          alert("js")
+//          break;
+//     default:
+//     alert("Wrong input")
+// }
+
+var a =1
+do{
+    console.log(a);
+    a++
+    
+}while(a<6)
