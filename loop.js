3745c0915ea92e0efb1ca5874807755091cb3509
Aryanmridha13 <aryanmridha8@gmail.com>
2024-12-13 19:54:28 +0530

    js query

--- loop.js
+++ loop.js
@@ -1,10 +1,6 @@
 //  let a =1
 
  for (let a = 1; a < 4; a++) {
-    for (let j = 0; j<4; j++) {
-        console.log(j);
-        
-        
-    }
-    
+   console.log(a);
+   
  }
---
+++ query.js
@@ -0,0 +1,9 @@
+let  h =document.querySelector('#head')
+h.style.color="blue"
+let h2=document.querySelector('#head2')
+h2.style.color="red"
+
+let b=document.getElementsByClassName('name')
+b[0].textContent="ddddddd"
+let a =document.querySelectorAll('h1')
+a[0]
