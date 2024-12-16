2b7f5faf21ba3a0a6db0c9e67a1c7c8ef20a57b1
Aryanmridha13 <aryanmridha8@gmail.com>
2024-12-16 19:59:06 +0530

    js even

---
+++ event.js
@@ -0,0 +1,36 @@
+function fun(){
+    alert("One click iis worked...!!!")
+}
+function add(){
+    let a =10
+    let b =20
+document.querySelector("#sum").innerHTML=a+b
+
+
+}
+function sud(){
+    let c=10
+    let d=5
+    document.querySelector("#sub").innerHTML=c-d
+}
+function mul(){
+    let e =10
+    let f =20
+document.querySelector("#mul").innerHTML=e*f
+
+}
+function div(){
+    let g =10
+    let h =2
+document.querySelector("#div").innerHTML=g/h
+
+}
+function red(){
+    let m = document.body.style.backgroundColor="red"
+}
+function blue(){
+    let m = document.body.style.backgroundColor="blue"
+}
+function yellow(){
+    let m = document.body.style.backgroundColor="yellow"
+}
