514d44819fe9a627fad14abe19bc96a1e747b80f
Aryanmridha13 <aryanmridha8@gmail.com>
2024-12-17 19:58:39 +0530

    js event

---
+++ event2.js
@@ -0,0 +1,29 @@
+function fun1(){
+    let img = document.getElementById('chimg')
+    img.src="images/newmain.jpg"
+}
+function fun2(){
+    let img = document.getElementById('chimg')
+    img.src="items/download.jpeg"
+}
+
+function demofun(){
+    let he1= document.querySelector('#head')
+    he1.classList.add('he1')
+}
+
+function rem(){
+    let he1 = document.getElementById('head')
+    he1.classList.remove('he1')
+}
+
+function btnone(){
+        let he1 = document.getElementById('head')
+        he1.classList.toggle('he1')
+    
+}
+
+function theme(){
+    let bd = document.body
+    bd.classList.toggle('theme')
+}
