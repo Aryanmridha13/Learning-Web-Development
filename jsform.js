bc04362654971ac8778105165bb8e0ad199af119
Aryanmridha13 <aryanmridha8@gmail.com>
2024-12-19 19:55:58 +0530

    js form

---
+++ jsform.js
@@ -0,0 +1,15 @@
+function add(){
+    let num1= parseInt(document.querySelector('#num1').value)
+    let num2= parseInt(document.querySelector('#num2').value)
+    let ans = document.querySelector('#ans')
+    // alert(num1+num2)
+    // document.body.innerHTML=(num1+num2)
+    ans.innerHTML=(num1+num2)
+    //  let bd = document
+    // let h2 = document.createElement('h1')
+    // h2.textContent =num1+num2
+    // bd.append(h2)
+    
+    return false
+
+}
