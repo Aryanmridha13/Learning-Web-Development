1ce59142edcee512ca9969e93fb65a4f70fe6533
Aryanmridha13 <aryanmridha8@gmail.com>
2024-12-27 19:12:14 +0530

    js event

---
+++ event3.js
@@ -0,0 +1,7 @@
+function fun(){
+    let h1 = document.querySelector('#head')
+     h1.innerHTML="cybrom"
+   
+    h1.classList.toggle('#head')
+
+}
--- eventlistener.js
+++ eventlistener.js
@@ -5,8 +5,15 @@ btn.addEventListener('dblclick',()=>alert("clicked"))
 
 let h1=document.querySelector('#head')
 let btn2 = document.querySelector('#btn2')
-btn2.addEventListener('click',()=>{h1.innerHTML="cybrom"})
+let c= btn2.addEventListener('click',()=>{h1.innerHTML="cybrom"})
 
 let btn3 = document.querySelector('#btn3')
 btn3.addEventListener('click',()=>{h1.innerHTML=""})
 
+function fun(){
+    let btn4 = document.querySelector('#btn4')
+    btn4.addEventListener('click',()=>{h1.innerHTML="cybrom"})
+
+   
+    
+}
