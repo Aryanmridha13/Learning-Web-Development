29b801db1168e1985b8eb26045797f3cb52a319d
Aryanmridha13 <aryanmridha8@gmail.com>
2024-12-30 19:49:41 +0530

    js function

---
+++ spr.js
@@ -0,0 +1,29 @@
+
+//spread operator
+let ar1 =[1,2,3]
+let ar2= [4,5,6]
+
+let ar3 = [...ar1,...ar2]
+
+console.log(ar3);
+
+
+let ar=[1,3,4,5,6,7,8,9,0]
+console.log(ar);
+
+//slice function
+
+// let del =ar.slice(1,5)
+// console.log(del);
+
+// console.log(ar);
+
+//splice function add and delete function
+
+ar.splice(1,5,"Code","exam","papper")
+console.log(ar);
+
+
+
+
+
