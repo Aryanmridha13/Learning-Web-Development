4ccc20606ad2e64becfa68c5e50c3105b3472f0f
Aryanmridha13 <aryanmridha8@gmail.com>
2024-12-31 19:55:18 +0530

    js obj

---
+++ form3.js
@@ -0,0 +1,16 @@
+function fun(){
+   let ob={
+    name : document.querySelector('#name').value,
+    age : document.querySelector('#age').value,
+    adress : document.querySelector('#adress').value,
+    feild : document.querySelector('#filed').value,
+    contact: document.querySelector('#contact').value,
+
+
+   }
+
+   console.log(ob);
+
+   return false
+   
+}
---
+++ obj.js
@@ -0,0 +1,13 @@
+let ob ={
+    name : "Aryan",
+    age : 19,
+    stack : "fsd"
+
+
+}
+console.log(ob.name);
+
+for(let k in ob){
+    console.log(ob[k]);
+    
+}
