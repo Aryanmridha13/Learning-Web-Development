3035a4f91aec72643a8694e6ed287bb680f88db5
Aryanmridha13 <aryanmridha8@gmail.com>
2025-02-11 19:58:32 +0530

    json

--- json/index.js
+++ json/index.js
@@ -1,3 +1,36 @@
+async function apifetch() {
+    let data = await fetch ("http://localhost:3000/student")
+    let final_data =await data.json()
+    let tdata = final_data.map((e)=>`
+       <tr>
+         <td> ${e.name}</td>
+         <td> ${e.age}</td>
+         <td> ${e.contact}</td>
+          <td> ${e.city}</td>
+         
+         <td> ${e.id}</td>
+         <td> <button onclick="mydelete('${e.id}')">Delete</button></td>
+         <td> <button onclick="myedit('${e.id}')">edit</button></td>
+      </tr>
+    
+    `).join("")
+    document.querySelector("#displydata").innerHTML=tdata
+
+
+}
+apifetch()
+
+function mydelete(id){
+  fetch(`http://localhost:3000/student/${id}`,{
+    method:"DELETE"
+  })
+  .then(res=>alert("Delete..!!"))
+}
+
+
+
+
+
 function savadata(){
     let frmdata={
         name : document.querySelector('#name').value,
@@ -18,4 +51,43 @@ body:JSON.stringify(frmdata)
 
 return false;
 
+}
+
+ async function myedit(id){
+    let data = await fetch(`http://localhost:3000/student/${id}`)
+    let resdata = await data.json()
+
+    let frm =`
+    <input type="text"  value="${resdata.id}" id="id1"  readonly><br><br>
+    <input type="text"  value="${resdata.name}" id="name1" ><br><br>
+    <input type="text"  value="${resdata.age}" id="age1"  ><br><br>
+    <input type="text"  value="${resdata.contact}" id="contact1" ><br><br>
+    <input type="text"  value="${resdata.city}" id="city1" ><br><br>
+    <input type="submit" value="Update" onclick="finalupdate('${resdata.id}')">
+    `
+
+    document.querySelector("#editfrm").innerHTML=frm
+}
+
+
+function finalupdate(){
+    let frmdata = {
+        id : document.querySelector('#id1').value,
+        name : document.querySelector('#name1').value,
+        age : document.querySelector('#age1').value,
+        contact : document.querySelector('#contact1').value,
+        city : document.querySelector('#city1').value,
+    }
+
+    fetch(`http://localhost:3000/student/${id}`,{
+        method:"PUT",
+        headers:{
+            'Content-type':'application/json'
+        },
+        body:JSON.stringify(frmdata)
+        })
+        .then(r=>alert("data update sucsefully.....!!!!"))
+    
+
+    
 }
